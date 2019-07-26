9b27cd5523639bf19d140cd27cedf02f5ea61dfe
nisarhassan12 <syednisarhassan12@gmail.com>
2019-07-26 11:34:37 +0000

    Updated user feedback section.

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import TweetEmbed from 'react-tweet-embed'
 import styled from '@emotion/styled'
+import { breakpoints } from '../utils/variables'
+
 
 const Tweets = styled.div`
     display: flex;
-    overflow-x: scroll;
+    flex-direction: column;
     margin-top: 4rem;
+
+    @media(min-width: ${breakpoints.md}) {
+       flex-direction: row;
+    }
 
     .tweet {
         &:not(:last-child) {
             margin-right: 4rem;
+        }
+
+        @media(max-width: ${breakpoints.md}) {
+            display: flex;
+            justify-content: center;
+
+            &:not(:last-child) {
+                margin-right: 0
+            }
         }
     }
 `
@@ -23,13 +38,11 @@ const Feedback = () => {
                     <h2>Community Feedback</h2>
                     <p>This is how contributors, maintainers and community leaders perceived the one-click experience with Gitpod.</p>
                 </div>
+                <Tweets>
+                    <TweetEmbed className="tweet" id='1115274432958930946' options={twitterOptions} />
+                    <TweetEmbed className="tweet" id='1130775701714558982' options={twitterOptions} />
+                </Tweets>
             </div>
-            <Tweets>
-                    <TweetEmbed className="tweet" id='1130775701714558982' options={twitterOptions} />
-                    <TweetEmbed className="tweet" id='1115274432958930946' options={twitterOptions} />
-                    <TweetEmbed className="tweet" id='1130775701714558982' options={twitterOptions} />
-                    <TweetEmbed className="tweet" id='1115274432958930946' options={twitterOptions} />
-            </Tweets>
         </section>
     )
 }
