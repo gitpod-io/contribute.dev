ff5db9bd77aaeb402c69f57886c355ca30c8811f
nisarhassan12 <syednisarhassan12@gmail.com>
2019-08-05 12:01:25 +0000

    Updated the site description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
         title: 'Contribute.dev',
-        description: 'On a mission to Open Source Contribution as convinient as possible.'
+        description: "Want to contribute to Open Source, but don't know where to start? Here is a list of really cool repositories where you can get started without any friction, in a single click."
     },
     plugins: [
         'gatsby-plugin-emotion'
