37e9ae4e580fc9599eda8d63de5c10bae7ad460b
Jan Keromnes <jan.keromnes@typefox.io>
2019-10-07 10:45:27 +0000

    Add GitHub repository link to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import GithubMark from '../resources/githubmark.png'
 
 const StyledFooter = styled.footer`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     width: 80%;
     margin: 0 auto 6rem;
     padding-top: 4rem;
     text-align: center;
+
+    & img {
+        height: 2rem;
+    }
 `
 
 const Footer = () => (
     <StyledFooter>
-            <p>© 2019 by <a href="https://typefox.io" target="_blank" rel="noopener">TypeFox</a></p>
+            <p><a href="https://github.com/gitpod-io/contribute.dev" target="_blank" rel="noopener" title="contribute.dev repository" aria-label="contribute.dev repository"><img src={GithubMark} alt="Github Mark" /></a></p>
+            <p>© 2019 by <a href="https://www.typefox.io/" target="_blank" rel="noopener">TypeFox</a></p>
     </StyledFooter>
 )
 
