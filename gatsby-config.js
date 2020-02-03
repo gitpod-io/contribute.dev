44840e6fbe6c12ad94907cdff3d9cd767b63c0b6
nisarhassan12 <syednisarhassan12@gmail.com>
2020-02-03 20:23:46 +0500

    wipe out old files.

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    siteMetadata: {
-        title: 'Contribute.dev',
-        description: "You can contribute to all repositories listed here with one click. No need to install dependencies, go through long README files or compile code for hours."
-    },
-    plugins: [
-        'gatsby-plugin-emotion'
-    ]
-}
