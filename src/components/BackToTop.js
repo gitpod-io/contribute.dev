181fbd54ad8d4cd369c03533889e98f63e21637b
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-05-13 00:40:41 +0000

    [back-to-top] add hover effect + display when the user has scrolled a bit down.

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import styled from '@emotion/styled'
 import ArrowUp from '../resources/arrow-up.svg'
@@ -17,6 +17,11 @@ const StyledBackToTop = styled(Link)`
     background: ${colors.purple};
     border-radius: 50%;
     box-shadow: 0 .5rem 1rem rgba(0,0,0, .2);
+    transition: transform .4s, opacity .4s, box-shadow .2s;
+
+    &:hover {    
+        box-shadow: 0 .3rem .6rem rgba(0,0,0, .3);
+    }
 
     img {
         height: 4rem;
@@ -24,8 +29,29 @@ const StyledBackToTop = styled(Link)`
 `
 
 const BackToTop = () => {
+    const [isRendered, setIsRendered] = useState(false)
+
+    const scroll = () => 
+        window.scrollY > 600 ? setIsRendered(true) : setIsRendered(false)
+
+    useEffect(() => {
+        window.addEventListener('scroll', scroll)
+
+        return () => {
+            window.removeEventListener('scroll', scroll)
+        }
+    })
+
     return (
-        <StyledBackToTop to="#top" title="Back to Top">
+        <StyledBackToTop 
+            to="#top" 
+            title="Back to Top" 
+            style={{
+                visibility: isRendered ? 'visible' : 'none',
+                transform: isRendered ? 'scale(1)' : 'scale(0)',
+                opacity: isRendered ? 1 : 0
+            }}
+        >
             <img src={ArrowUp} alt="Back to Top"/>
         </StyledBackToTop>
     )
