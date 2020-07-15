3290cd6bbac8cb5c47d03f10c661c006e0daa1bf
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-07-15 11:50:56 +0000

    adjust the layout.

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -5,30 +5,18 @@ import { breakpoints, grid } from '../utils/variables'
 
 const Tweets = styled.div`
     display: flex;
-    flex-direction: row;
-    overflow-x: scroll;
+    flex-wrap: wrap;
+    justify-content: space-between;
     padding: 3rem 0;
 
-    @media(min-width: calc(${breakpoints.md} + 1px)) {
-        padding-left: calc((100% - ${grid.maxWidth})/2);
-    }
-    
-    & > div {
-        &:first-of-type {
-            @media(max-width: ${breakpoints.md}) {
-                padding-left: 4rem;
-            }
-            @media(max-width: ${breakpoints.sm}) {
-                padding-left: 1rem;
-            }
-        }
+    @media(max-width: ${breakpoints.md}) {
+        flex-direction: column;
     }
 
     .tweet {
-        max-width: 90%;
-
-        &:not(:last-child) {
-            margin-right: 3rem;
+        flex: 0 0 49%;
+        @media(max-width: ${breakpoints.md}) {
+            width: 100%;
         }
     } 
 `
@@ -42,14 +30,12 @@ const Feedback = () => {
                     <h2>Community Feedback</h2>
                     <p style={{ textAlign: "center" }}>This is how contributors, maintainers and community leaders perceived the one-click experience with Gitpod.</p>
                 </div>
+                <Tweets>
+                    <TweetEmbed className="tweet" id='1115274432958930946' options={twitterOptions} />
+                    <TweetEmbed className="tweet" id='1167463499779338243' options={twitterOptions} />
+                    <TweetEmbed className="tweet" id='1130775701714558982' options={twitterOptions} />
+                </Tweets>
             </div>
-            <Tweets>
-                <TweetEmbed className="tweet" id='1115274432958930946' options={twitterOptions} />
-                <TweetEmbed className="tweet" id='1167463499779338243' options={twitterOptions} />
-                <TweetEmbed className="tweet" id='1130775701714558982' options={twitterOptions} />
-                <TweetEmbed className="tweet" id='1167463499779338243' options={twitterOptions} />
-                <TweetEmbed className="tweet" id='1130775701714558982' options={twitterOptions} />
-            </Tweets>
         </section>
     )
 }
