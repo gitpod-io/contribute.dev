7d53f5fc49755bd73a6326c2a107501fc18db14b
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-07-16 05:25:57 +0000

    improve page title.
    Fixes gitpod-io/contribute.dev#81

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-        title: 'Contribute.dev',
+        title: 'Contribute.dev | list of cool open source projects that you can contribute to with one click!',
         description: "You can contribute to all repositories listed here with one click. No need to install dependencies, go through long README files or compile code for hours."
     },
     plugins: [
