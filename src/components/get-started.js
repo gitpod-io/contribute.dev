c0d19b5818729db76eec1b8e83aa835aa9d09c92
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-10-06 03:11:28 +0000

    add the get started section.

--- src/components/get-started.js
+++ src/components/get-started.js
@@ -2,24 +2,84 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 import Illustration from '../resources/illustration-grid.jpg';
+import IllustrationSmall from '../resources/illustration-small.jpg';
+import { breakpoints, colors, shadows } from '../utils/variables';
 
 const StyledGetStarted = styled.section`
-    & > div {
+    .container {
         display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: #f9f9f9;
+        border-radius: 6.4rem;
+        box-shadow: ${shadows.normal};
+
+        @media(max-width: 860px) {
+            flex-direction: column;
+            text-align: center;
+            max-width: 500px;
+            margin: 0 auto;
+        }
+    }
+
+    .text {
+        flex: 0 0 40%;
+        padding: 5rem 1rem 5rem 7rem ;
+        
+        @media(max-width: 860px) {
+            padding: 5rem;
+            max-width: 400px;
+        }
+
+        @media(max-width: ${breakpoints.sm}) {
+            padding: 5rem 2rem;
+        }
+    }
+
+    .img-container {
+        flex: 1;
+        
+        @media(max-width: 860px) {
+            padding: 0 5rem 5rem;
+        }
+    }
+
+    h2 {
+       @media(min-width: 861px) {
+        text-align: left;
+
+        &::after {
+            margin-left: 0;
+        }
+       }
+    }
+
+    img {
+        display: block;
+        object-fit: contain;
+        max-width: 100%;
+        
+        @media(min-width: 861px) {
+            border-top-right-radius: 6.4rem;
+            border-bottom-right-radius: 6.4rem;
+        }
     }
 `;
 
 const GetStarted = () => (
     <div className="row">
         <StyledGetStarted>
-            <div>
-                <div>
+            <div className="container">
+                <div className="text">
                     <h2>Get Started</h2>
                     <p>Spin up fresh, automated dev environments for each task, in the cloud, in seconds.</p>
-                    <a href="https://www.gitpod.io/" className="btn">Try Now</a>
+                    <a href="https://www.gitpod.io/#get-started" className="btn">Try Now</a>
                 </div>
-                <div>
-                    <img src={Illustration} alt="Gitpod in a Nutshell" />
+                <div className="img-container">
+                    <picture>
+                        <source srcSet={IllustrationSmall} media="(max-width: 860px)" />
+                        <img src={Illustration} alt="Gitpod in a Nutshell" />
+                    </picture>
                 </div>
             </div>
         </StyledGetStarted>
